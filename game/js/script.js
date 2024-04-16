e3c7bfc98035e896278d54a5324a3c3de7e52f30
Shubham Bharat Gaikwad <90133206+Shubham7191@users.noreply.github.com>
2024-04-16 08:08:38 +0530

    Add files via upload

---
+++ game/js/script.js
@@ -0,0 +1,7 @@
+
+
+
+    var Animals =["cat" ,"dog" ,"tiger","monkey","zebra"];
+    var Fruits = ["apple" ,"mango" ,"banana" ,"straberrey","cherry"];
+
+var letterBoard =document.getElementById("LetterBoard");
